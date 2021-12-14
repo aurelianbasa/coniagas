4e9173dcb87b3422f03defd88ba5ff9548a9d166
ElegantStack <elegantstack.studio@gmail.com>
2021-12-14 07:27:02 -0800

    add table sample page

---
+++ site/src/pages/table.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Container } from 'theme-ui'
+import Helmet from 'react-helmet'
+import Layout from '@solid-ui-layout/Layout'
+
+const TablePage = props => {
+  return (
+    <Layout {...props}>
+      <Helmet>
+        <style type='text/css'>{`
+          table {
+            width:100%;
+            border:1px solid #b3adad;
+            border-collapse:collapse;
+            padding:5px;
+          }
+          table th {
+            border:1px solid #b3adad;
+            padding:5px;
+            background: #f0f0f0;
+            color: #313030;
+          }
+          table td {
+            border:1px solid #b3adad;
+            text-align:center;
+            padding:5px;
+            background: #ffffff;
+            color: #313030;
+          }
+      `}</style>
+      </Helmet>
+      <Container>
+        <table>
+          <thead>
+            <tr>
+              <th>Header 1</th>
+              <th>Header 2</th>
+              <th>Header 3</th>
+              <th>Header 4</th>
+              <th>Header 5</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Hello</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>Testing</td>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>Aurelian</td>
+              <td>3</td>
+              <td>5</td>
+              <td>7</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Table</td>
+              <td>22</td>
+              <td>7</td>
+              <td>3</td>
+              <td>5</td>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
+    </Layout>
+  )
+}
+
+export default TablePage
