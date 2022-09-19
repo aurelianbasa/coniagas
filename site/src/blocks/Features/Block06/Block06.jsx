3ef21b27a08295a64075cf05019bb20b7c887fb9
Aurelian Basa <aurelianbasa@live.ca>
2022-09-19 14:49:33 -0400

    Update Block06.jsx

--- site/src/blocks/Features/Block06/Block06.jsx
+++ site/src/blocks/Features/Block06/Block06.jsx
@@ -1,188 +1,102 @@
 import React from 'react'
-import { Container, Flex, Box, Badge, Link } from 'theme-ui'
-import ContentContainer from '@solid-ui-components/ContentContainer'
-import Reveal from '@solid-ui-components/Reveal'
+import { Container, Box, Flex, css } from 'theme-ui'
 import Divider from '@solid-ui-components/Divider'
+import Reveal from '@solid-ui-components/Reveal'
 import ContentText from '@solid-ui-components/ContentText'
 import ContentImages from '@solid-ui-components/ContentImages'
 import ContentButtons from '@solid-ui-components/ContentButtons'
 import WithDefaultContent from '@solid-ui-blocks/WithDefaultContent'
 
-import { FaRegClock } from 'react-icons/fa'
-
-const styles = {
-  wrapper: {
-    alignItems: `stretch`,
-    flexDirection: [`row`],
-    height: `full`
-  },
-  card: {
-    height: `full`
-  },
-  content: {
-    alignItems: `stretch`,
-    flexDirection: [`row`, null, null, `column`],
-    height: `full`
-  },
-  body: {
-    display: `flex`,
-    flexDirection: `column`,
-    justifyContent: `center`,
-    flex: 1,
-    p: 4
-  },
-  footerWrapper: {
-    alignItems: `center`
-  },
-  postInfo: {
-    flex: 1,
-    flexWrap: `wrap`,
-    justifyContent: `space-between`,
-    color: `omega`,
-    ml: 3
-  },
-  imageWrapper: {
-    textAlign: `center`,
-    position: `relative`,
-    display: `block`,
-    height: `full`
-  },
-  image: {
-    display: [`block`],
-    height: `full`,
-    bg: `omegaLighter`,
-    borderRadius: `default`
-  },
-  avatar: {
-    size: 42,
-    bg: `omegaLighter`,
-    borderRadius: `full`,
-    borderStyle: `solid`,
-    borderWidth: `md`,
-    borderColor: `omegaLighter`,
-    boxSizing: `content-box`,
-    img: {
-      objectPosition: 'top center !important'
-    }
-  }
-}
-
-const BlogBlock01 = ({ content: { text, collection, buttons } }) => (
+const TeamsBlock01 = ({
+  content: { container, text, images, buttons, collection }
+}) => (
   <Container>
-    <Box sx={{ textAlign: `left` }}>
-      <ContentText content={text} />
-    </Box>
-    <Divider />
-    {collection && (
-      <Reveal effect='fadeIn'>
-        <Flex sx={{ justifyContent: `center`, flexWrap: `wrap`, m: 3 }}>
-          {collection.map(
-            ({ container, text, images, avatar, buttons }, index) => (
-              <Box key={`item-${index}`} sx={styles.wrapper}>
-                <ContentContainer
-                  content={container}
-                  variant='cards.interactive'
-                  sx={styles.card}
-                >
-                  <Flex as='article' sx={styles.content}>
-                    {/* Image */}
-                    <Box sx={{ flex: [0, 1], m: 2, mb: [null, null, null, 0] }}>
-                      <Box sx={styles.imageWrapper}>
-                        <ContentImages
-                          content={{ images }}
-                          sx={styles.image}
-                          imageEffect='fadeIn'
-                        />
-                      </Box>
-                    </Box>
-                    <Box sx={styles.body}>
-                      {/* Category */}
-                      {text?.[0]?.text && (
-                        <Box sx={{ display: `inline-block` }}>
-                          <Box mb='3'>
-                            <Badge
-                              variant='tag'
-                              sx={{ bg: `alphaLighter` }}
-                              color={text[0]?.color}
-                            >
-                              {text[0].text}
-                            </Badge>
-                          </Box>
-                        </Box>
-                      )}
-                      {/* Title */}
-                      <ContentText
-                        content={text?.[1]}
-                        sx={{ flex: [0, 0, `auto`] }}
-                      />
-                      {/* Excerpt */}
-                      <ContentText
-                        content={text?.[2]}
-                        variant='small'
-                        sx={{ flex: `auto`, mb: 3 }}
-                      />
-                      {/* Footer */}
-                      <Box sx={{ variant: `text.small` }}>
-                        <Flex sx={styles.footerWrapper}>
-                          <ContentImages
-                            content={{ images: [avatar] }}
-                            sx={styles.avatar}
-                            imageEffect='fadeIn'
-                          />
-                          <Flex sx={styles.postInfo}>
-                            {/* Author */}
-                            <ContentText
-                              content={text?.[3]}
-                              sx={{
-                                display: `inline-block`,
-                                flexBasis: `full`
-                              }}
-                            >
-                              <Link color={text?.[3]?.color}>
-                                <strong>{text?.[3]?.text}</strong>
-                              </Link>
-                            </ContentText>
-                            {/* Info */}
-                            <Flex sx={{ alignItems: `center` }}>
-                              <ContentText
-                                content={text?.[4]}
-                                sx={{ display: `inline-block` }}
-                                mr='2'
-                              >
-                                {text?.[4]?.textGroup?.[0]}
-                              </ContentText>
-                              {text?.[5] && <FaRegClock />}
-                              <ContentText
-                                content={text?.[5]}
-                                sx={{ display: `inline-block` }}
-                                ml='2'
-                              />
-                            </Flex>
-                          </Flex>
-                        </Flex>
-                      </Box>
-                      {buttons && (
-                        <>
-                          <Divider space={2} />
-                          <ContentButtons content={buttons} />
-                        </>
-                      )}
-                    </Box>
-                  </Flex>
-                </ContentContainer>
-              </Box>
-            )
+    <Flex
+      sx={{
+        alignItems: `center`,
+        flexDirection: [`column`, null, null, `row`]
+      }}
+    >
+      <Box
+        sx={{
+          flexBasis: `1/2`,
+          textAlign: `center`,
+          mr: [null, null, null, 5]
+        }}
+      >
+        <Reveal effect='fadeInDown'>
+          {images?.[0]?.src && (
+            <>
+              <ContentImages content={{ images }} />
+              <Divider space={3} />
+            </>
           )}
-        </Flex>
-      </Reveal>
-    )}
-    {buttons && (
-      <>
-        <Divider space={3} />
-        <ContentButtons content={buttons} />
-      </>
-    )}
+          <ContentText content={text} />
+        </Reveal>
+      </Box>
+      <Box
+        sx={{
+          flexBasis: `1/2`,
+          mt: [4, null, null, 0],
+          ml: [null, null, null, 5]
+        }}
+      >
+        <Reveal effect='fadeInLeft'>
+          <Box
+            content={container}
+            variant='cards.paper'
+            sx={{
+              textAlign: `center`,
+              mx: `auto`,
+              background: container?.bg
+            }}
+          >
+            <Divider space={2} />
+            <ContentText content={text} variant='h5' />
+            <Divider space={1} />
+            <Flex sx={{ flexWrap: `wrap`, justifyContent: `center` }}>
+              {collection?.map(({ text, avatar }, index) => (
+                <Reveal
+                  effect='fadeInRotate'
+                  delay={1 + 0.2 * (index + 1)}
+                  css={css({ p: 2, boxSizing: 'content-box' })}
+                >
+                  <ContentImages
+                    content={{ images: [avatar] }}
+                    imageEffect='fadeIn'
+                    sx={{
+                      size: 100,
+                      mx: `auto`,
+                      img: {
+                        bg: `omegaLight`,
+                        borderRadius: `full`
+                      }
+                    }}
+                  />
+                  {text?.[0] && (
+                    <Reveal delay={1.6 + 0.2 * (index + 1)}>
+                      <ContentText
+                        content={text?.[0]}
+                        variant='badges.tag'
+                        sx={{ position: `relative`, zIndex: 2, mt: -4 }}
+                      />
+                    </Reveal>
+                  )}
+                </Reveal>
+              ))}
+            </Flex>
+            {buttons && (
+              <>
+                <Divider space={3} />
+                <ContentButtons content={buttons} />
+              </>
+            )}
+            <Divider space={2} />
+          </Box>
+        </Reveal>
+      </Box>
+    </Flex>
   </Container>
 )
 
-export default WithDefaultContent(BlogBlock01)
+export default WithDefaultContent(TeamsBlock01)
