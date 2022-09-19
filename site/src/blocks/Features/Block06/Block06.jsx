37f7a0ce021c9bf59d7aa4eff9f1d33a3f2c4f47
Aurelian Basa <aurelianbasa@live.ca>
2022-09-19 14:17:07 -0400

    Update Block06.jsx

--- site/src/blocks/Features/Block06/Block06.jsx
+++ site/src/blocks/Features/Block06/Block06.jsx
@@ -48,7 +48,7 @@ const styles = {
     height: `full`
   },
   image: {
-    display: [`none`, `block`],
+    display: [`block`],
     height: `full`,
     bg: `omegaLighter`,
     borderRadius: `default`,
@@ -79,7 +79,7 @@ const BlogBlock01 = ({ content: { text, collection, buttons } }) => (
     <Divider />
     {collection && (
       <Reveal effect='fadeIn'>
-        <Flex sx={{ flexBasis: auto, p: 3 }}>
+        <Flex sx={{ flexBasis: [`1/2`], p: 3 }}>
           {collection.map(
             ({ container, text, images, avatar, buttons }, index) => (
               <Box key={`item-${index}`} sx={styles.wrapper}>
