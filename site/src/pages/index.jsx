a751d581dae8a7051873baec05ff7ee45c48ddd1
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-07-26 22:15:39 -0400

    Update index.jsx

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -30,8 +30,8 @@ const IndexPage = props => {
       <ModalSimple content={content['disclaimer']} />
       {/* Blocks */}
       <Header content={content['header']} />
-      <Divider space='4' />
-      <Divider space='4' />
+      <Divider space='3' />
+      <Divider space='3' />
       <Container variant='wide' sx={styles.heroContainer}>
       <Hero content={content['hero']} reverse />
       </Container>
