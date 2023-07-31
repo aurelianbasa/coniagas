062768775f91450353324c363352177d942b7209
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-07-31 12:06:46 -0400

    Update gatsby-config.js

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       }
     },
     {
-      resolve: '@belegantstack/gatsby-theme-flexiblog-science',
+      resolve: '@elegantstack/gatsby-theme-flexiblog-science',
       options: {
         sources: {
           contentful: true,
