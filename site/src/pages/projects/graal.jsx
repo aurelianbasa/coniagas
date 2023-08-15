bd0fbd8b4247c8d6062a208f2b465f66cebf28b8
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-08-14 22:05:03 -0400

    big moves

--- site/src/pages/projects/graal.jsx
+++ site/src/pages/projects/graal.jsx
@@ -6,9 +6,12 @@ import Seo from '@solid-ui-components/Seo'
 import Divider from '@solid-ui-components/Divider'
 import ModalSimple from '@solid-ui-blocks/Modal/Block02'
 import Header from '@solid-ui-blocks/Header/Block01'
-import Info from '@solid-ui-blocks/FeaturesWithPhoto/Block01'
-import DrillMap from '../../blocks/DrillHolesMap/Block01'
+import Info from '@solid-ui-blocks/FeaturesWithPhoto/Block05'
+import Location from '@solid-ui-blocks/FeaturesWithPhoto/Block03'
 import Download from '@solid-ui-blocks/CallToAction/Block02'
+import Milestones from '@solid-ui-blocks/Features/Block01'
+import Stats from '@solid-ui-blocks/Stats/Block01'
+import Results from '@solid-ui-blocks/FeaturesWithPhoto/Block04'
 import Footer from '@solid-ui-blocks/Footer/Block01'
 import { normalizeBlockContentNodes } from '@blocks-helpers'
 
@@ -18,7 +21,7 @@ const ProjectItemPage = props => {
 
   return (
     <Layout {...props}>
-      <Seo title='The Graal Project' />
+      <Seo title='Nourricier Project' />
       {/* Modals */}
       <ModalSimple content={content['privacy-policy']} />
       <ModalSimple content={content['disclaimer']} />
@@ -27,17 +30,24 @@ const ProjectItemPage = props => {
       <Divider space='5' />
       <Divider space='5' />
       <Info content={content['overview']} />
+      <Divider space='2' />
+      <Download content={content['download']} />
       <Divider space='4' />
+      <Milestones content={content['milestones-1']} />
+      <Divider space='5' />
       <Container variant='narrow'>
-        <DrillMap content={content['drill-map']} />
-      </Container>
+      <Location content={content['location']} />
+        <Divider space='5' />
+        <Info content={content['info-3']} reverse />
+        <Divider space='5' />
+        </Container>
+      <Stats content={content['stats']} />
       <Divider space='5' />
       <Container variant='narrow'>
         <Info content={content['info-1']} reverse />
         <Divider space='5' />
         <Info content={content['info-2']} />
         <Divider space='5' />
-        <Download content={content['download']} />
       </Container>
       <Divider space='5' />
       <Footer content={content['footer']} />
@@ -46,9 +56,9 @@ const ProjectItemPage = props => {
 }
 
 export const query = graphql`
-  query siteProjectsGraalBlockContent {
+  query siteProjectsNourricierBlockContent {
     allBlockContent(
-      filter: { page: { in: ["site/projects/graal", "site/shared"] } }
+      filter: { page: { in: ["site/projects/nourricier", "site/shared"] } }
     ) {
       nodes {
         ...BlockContent
