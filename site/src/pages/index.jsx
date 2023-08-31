e5b672c1d11b2378b40d855c9d8babc287745794
Aurelian Basa <aurelianbasa@live.ca>
2023-08-31 11:22:50 -0400

    ready to edit

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -36,7 +36,7 @@ const IndexPage = props => {
       </Container>
       <Divider space='5' />
       <Divider space='5' />
-      <Divider space='3' />
+      <Divider space='5' />
       <Primary content={content['primary']} reverse />
       <Divider space='4' />
       <Divider space='5' />
