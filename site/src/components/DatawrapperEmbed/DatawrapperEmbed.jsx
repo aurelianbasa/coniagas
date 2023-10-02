51dd72635548f18096d7404351add4d64652b400
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-10-02 09:39:41 -0400

    what could happen now?

--- site/src/components/DatawrapperEmbed/DatawrapperEmbed.jsx
+++ site/src/components/DatawrapperEmbed/DatawrapperEmbed.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DatawrapperEmbed = ({ as: CustomComponent, content, ...props }) => {
     const { text } = content;
-    const src = text[0].text;
+    const src = text[1].text;
 
     return (
         <iframe src={src} frameborder="0" style={{width: '100%', minWidth: '100% !important', border: 'none'}} height="942" data-external="1" />
