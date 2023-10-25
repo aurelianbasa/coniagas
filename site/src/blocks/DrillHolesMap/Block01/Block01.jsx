4a3553b37524dfa28d40309d1b65ce095c330c38
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-10-25 09:57:46 -0400

    final adjustments before next upload

--- site/src/blocks/DrillHolesMap/Block01/Block01.jsx
+++ site/src/blocks/DrillHolesMap/Block01/Block01.jsx
@@ -104,7 +104,9 @@ const DrillHolesMapBlock01 = ({
                   <iframe style={styles.iframe} src={text[1]?.text} />
                 </Box>
                 <Divider space={3} />
+                <Box sx={styles.container}>
                 <ContentText content={text?.[2]} mb='0' pr='5' />
+                </Box>
               </AccordionItemPanel>
             </AccordionItem>
           </ContentContainer>
