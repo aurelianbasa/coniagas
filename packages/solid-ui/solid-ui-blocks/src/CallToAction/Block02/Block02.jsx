e3c7d400e259d9202c62be6b246ddd4a4ea60037
Aurelian Basa <aurelianbasa@live.ca>
2023-10-26 11:01:26 -0400

    Update Block02.jsx

--- packages/solid-ui/solid-ui-blocks/src/CallToAction/Block02/Block02.jsx
+++ packages/solid-ui/solid-ui-blocks/src/CallToAction/Block02/Block02.jsx
@@ -8,7 +8,7 @@ import WithDefaultContent from '@solid-ui-blocks/WithDefaultContent'
 
 const styles = {
   card: {
-    overflow: `hidden`,
+    width: `full`,
     bg: `beta`,
     height: `full`
   }
@@ -20,9 +20,9 @@ const CallToActionBlock02 = ({ content: { container, text, icon, buttons } }) =>
     <ContentContainer content={container} variant='cards.paper' sx={styles.card}>
         <Flex sx={{ alignItems: `left` }} p='3'>
                     <Icon content={icon} size='md' mr='3' p='2' round />
-                    <ContentText content={text} mb='0' pr='5' />
+                    <ContentText content={text} mb='2' pr='5' />
                     <ContentButtons content={buttons} 
-        sx={{ alignItems: `left` }}
+        sx={{ alignItems: `right` }}
         />
       </Flex>
     </ContentContainer>
