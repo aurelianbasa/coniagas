f01693f4e5388a0027cb16b7f720b69b4a877c3a
Aurelian Basa <aurelianbasa@live.ca>
2023-10-26 11:14:56 -0400

    widths of buttons

--- packages/solid-ui/solid-ui-blocks/src/CallToAction/Block02/Block02.jsx
+++ packages/solid-ui/solid-ui-blocks/src/CallToAction/Block02/Block02.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Flex } from 'theme-ui'
+import { Container, Flex, Box } from 'theme-ui'
 import ContentText from '@solid-ui-components/ContentText'
 import ContentContainer from '@solid-ui-components/ContentContainer'
 import ContentButtons from '@solid-ui-components/ContentButtons'
@@ -20,9 +20,11 @@ const CallToActionBlock02 = ({ content: { container, text, icon, buttons } }) =>
     <ContentContainer content={container} variant='cards.paper' sx={styles.card}>
         <Flex sx={{ flexWrap: `wrap` }} p='3'>
                     <Icon content={icon} size='md' mr='3' p='2' round />
+                    
                     <ContentText content={text} mb='2' />
+                    <Box>
                     <ContentButtons content={buttons}   mb='2' />
-      </Flex>
+      </Box></Flex>
     </ContentContainer>
   </Container>
 )
