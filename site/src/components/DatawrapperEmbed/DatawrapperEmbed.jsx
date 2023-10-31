53ad350c5fe2ff44e896bf843edc933ef926dc39
Ian Vasquez <5068223+intenseandnoble@users.noreply.github.com>
2023-10-31 10:17:02 -0400

    fix: Remove overlay on prod
    Attempt with useRef instead of querySelectorAll

--- site/src/components/DatawrapperEmbed/DatawrapperEmbed.jsx
+++ site/src/components/DatawrapperEmbed/DatawrapperEmbed.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DatawrapperEmbed = ({ as: CustomComponent, content, ...props }) => {
     const { text } = content;
     const src = text[1].text;
-
+    const dwRef = React.useRef(null);
     const isBrowser = typeof window !== "undefined";
 
     if (!isBrowser) {
@@ -11,8 +11,9 @@ const DatawrapperEmbed = ({ as: CustomComponent, content, ...props }) => {
     }
 
     return (
-        <div id={src} style={{ width: "100%" }}>
+        <div style={{ width: "100%" }}>
             <iframe
+                ref={dwRef}
                 src={src}
                 scrolling="no"
                 frameborder="0"
@@ -23,12 +24,11 @@ const DatawrapperEmbed = ({ as: CustomComponent, content, ...props }) => {
                     "use strict";
                     window.addEventListener("message", (function (a) {
                         if (void 0 !== a.data["datawrapper-height"]) {
-                            var e = document.querySelectorAll("iframe");
+                            var e = dwRef.current;
                             for (var t in a.data["datawrapper-height"])
-                                for (var r = 0; r < e.length; r++)
-                                    if (e[r].contentWindow === a.source) {
-                                        var i = a.data["datawrapper-height"][t] + "px"; e[r].style.height = i
-                                    }
+                                if (e.contentWindow === a.source) {
+                                    var i = a.data["datawrapper-height"][t] + "px"; e.style.height = i
+                                }
                         }
                     }
                     ))
