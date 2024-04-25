cccb280c924d8168abf9e061781537d4a3395d89
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2024-04-25 08:32:05 -0400

    better I hope

--- site/src/pages/investors/index.en.jsx
+++ site/src/pages/investors/index.en.jsx
@@ -5,14 +5,13 @@ import Layout from '@solid-ui-layout/Layout'
 import Seo from '@solid-ui-components/Seo'
 import Divider from '@solid-ui-components/Divider'
 import ModalSimple from '@solid-ui-blocks/Modal/Block02'
-import Content from '@solid-ui-blocks/Content/Block02'
+import Content from '@solid-ui-blocks/Content/Block01'
 import Header from '@solid-ui-blocks/Header/Block01'
 import Faq from '@solid-ui-blocks/Faq/Block02'
 import Table from '../../blocks/Table/Block01'
 import StockChart from '../../blocks/StockChart/Block01'
 import Footer from '@solid-ui-blocks/Footer/Block01'
 import Download from '@solid-ui-blocks/CallToAction/Block02'
-import Reload from '@solid-ui-blocks/CallToAction/Block01'
 import { normalizeBlockContentNodes } from '@blocks-helpers'
 import styles from './_styles'
 
@@ -41,7 +40,7 @@ const MediaPage = props => {
       <Divider space='5' />
       <Faq content={content['analyst-intro']} />
       <Divider space='1' />
-      <Reload content={content['analyst-coverage']} />
+      <Content content={content['analyst-coverage']} />
       <Divider space='5' />
       <Download content={content['download']} />
       <Divider space='5' />
